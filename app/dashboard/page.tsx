9328007ca76f9984e1d5c547ca8be03f31fd47ac
Narek Hovhannisyan <narek.hovhannisyan1.im@gmail.com>
2023-09-20 12:26:50 +0400

    add some placeholder pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us",
+  description: "About NextSpace",
+};
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Placeholder for dashboard page</p>
+    </div>
+  );
+}
---
+++ app/users/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us",
+  description: "About NextSpace",
+};
+
+export default function Users() {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>Placeholder for Users page</p>
+    </div>
+  );
+}
