a0c238f378aadffe1527fd00dcdded0075922233
Narek Hovhannisyan <narek.hovhannisyan1.im@gmail.com>
2023-09-22 10:35:19 +0400

    add dynamic routes for users

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { Metadata } from "next";
 
 interface Props {
   params: {
@@ -6,9 +7,31 @@ interface Props {
   };
 }
 
-// export default async function UserProfile({ params }: Props) {
-// const user = prisma.user.findUnique({ where: { id: params.id } });
-// const { name, bio, image}: {string|null, string|null, string|null} = user ?? {};
+interface User {
+  id: string;
+  name: string | null;
+  email: string | null;
+  emailVerified: Date | null;
+  image: string | null;
+  age: number | null;
+  bio: string | null;
+}
 
-//   return <div>{name}</div>;
-// }
+export default async function UserProfile({ params }: Props) {
+  const user: User = prisma.user.findUnique({ where: { id: params.id } });
+  const { name, bio, image } = user ?? {};
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img width={300} src={image ?? "/user.webp"} alt={`${name}s profile`} />
+      <h3>Bio</h3>
+      <p>{bio}</p>
+    </div>
+  );
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = prisma.user.findUnique({ where: { id: params.id } });
+  return { title: `User profile of ${user?.name}` };
+}
